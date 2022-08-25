3db695b6f02fdc4a6915fcef7c29cc0344bcbaaf
askipitaris <alexskipitaris@gmail.com>
2022-08-25 15:48:39 -0400

    pdfs can be scraped from s&p

--- application/webscraping/snp/scraping_snp.py
+++ application/webscraping/snp/scraping_snp.py
@@ -2,9 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 import os
 import re
+from datetime import datetime
 
 
-def download_file(url):
+def download_pdf(url):
     """
 
     :param url: The S&P page you want to download a pdf from.
@@ -23,19 +24,30 @@ def download_file(url):
     if not os.path.exists(path):
         os.makedirs(path)
 
-    file_name = os.path.join(dir_name, 'snp_pdfs', url.split('/')[-1] + '.pdf')
-    print(file_name)
+    endpoint = url.split('/')[-1]
+    file_date = endpoint.split('-')[0]
+    file_name = os.path.join(dir_name, 'snp_pdfs', endpoint + '.pdf')
 
-    for link in links:
-        item = link.get('href', [])
-        if '.pdf' in link.get('href', []):
-            response = requests.get('https://www.spglobal.com' + item)
-            pdf = open(file_name, 'wb')
-            pdf.write(response.content)
-            pdf.close()
-            break
+    if file_date.isdigit():
+        # We only want reports from the last 31 days, so we need to compare their
+        # publication date to current date
+        now = datetime.now()
+        pub_date = datetime.strptime(str(file_date), '%y%m%d')
+        pub_age = now - pub_date
 
-    return file_name + ' downloaded'
+        if pub_age.days <= 31:
+            for link in links:
+                item = link.get('href', [])
+                if '.pdf' in link.get('href', []):
+                    response = requests.get('https://www.spglobal.com' + item)
+                    pdf = open(file_name, 'wb')
+                    pdf.write(response.content)
+                    pdf.close()
+                    return 1
+        else:
+            return 0
+    else:
+        return 0
 
 
 # https://stackoverflow.com/questions/15175142/how-can-i-do-multiple-substitutions-using-regex
@@ -96,4 +108,20 @@ def get_documents(industry):
 
     """
     target = update_provider_url(industry)
-    return target
+    response = requests.get(target)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    links = soup.find_all('a')
+
+    files_downloaded = 0
+    for link in links:
+        item = link.get('href', [])
+        if (
+                isinstance(item, str)
+                and len(item.split('/')) > 3
+                and item.split('/')[3] == 'research'
+                and item.split('/')[4] == 'pdf-articles'
+                and not item.split('/')[5].__contains__('presale')
+        ):
+            files_downloaded = files_downloaded + download_pdf('https://www.spglobal.com' + item)
+
+    return files_downloaded
